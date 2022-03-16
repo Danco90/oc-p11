e18b2f5610b8929043fa6c9e8404b3d2b92d222a
Danco90 <danielematteo90@gmail.com>
2022-03-16 19:40:12 +0100

    Added enum methods to abstract factory examples

--- src/com/pino/project/ocp11/java11/ocp/chapter12/javafundamentals/designpatterns/creational/abstractfactory/AbstractDesignManager.java
+++ src/com/pino/project/ocp11/java11/ocp/chapter12/javafundamentals/designpatterns/creational/abstractfactory/AbstractDesignManager.java
@@ -5,9 +5,11 @@ public class AbstractDesignManager {
     public static void main(String...args)
     {
         System.out.println("All the Car types : ");
-        for(CarType carType: CarType.values())
-            System.out.print(carType+", ");
-        
+        for(CarType carType: CarType.values()) {
+            System.out.print(carType + " "+carType.ordinal()+", ");
+            carType.printDetails();
+        }
+
         System.out.println(CarFactory.buildCar(CarType.MICRO));
         System.out.println(CarFactory.buildCar(CarType.MINI));
         System.out.println(CarFactory.buildCar(CarType.LUXURY));
