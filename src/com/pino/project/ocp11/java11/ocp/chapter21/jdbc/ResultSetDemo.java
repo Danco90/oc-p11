f30166e45ff8b3644e8203cc86d92f567aeed8a3
matteodaniele <danielematteo90@gmail.com>
2022-10-13 18:34:52 +0100

    Removed emtpy lines for testing java 19 config

--- src/com/pino/project/ocp11/java11/ocp/chapter21/jdbc/ResultSetDemo.java
+++ src/com/pino/project/ocp11/java11/ocp/chapter21/jdbc/ResultSetDemo.java
@@ -1,14 +1,13 @@
 package com.pino.project.ocp11.java11.ocp.chapter21.jdbc;
 
-import java.sql.ResultSet;
+//import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ResultSetDemo
 {
     public static void main (String...args)
-    {
-        String sql = "SELECT id, name FROM exhibits";
+    {   String sql = "SELECT id, name FROM exhibits";
         Map<Integer, String> idToNameMap = new HashMap<>();
 
        // try (var ps =  conn.prepareStatement(sql);
@@ -18,8 +17,5 @@ public class ResultSetDemo
 
             //}
         //}
-
-
-
     }
 }
